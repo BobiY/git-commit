6b8d2a0b417bbd50fb926194681330445a379d2c
wb-ysb252760 <wb-ysb252760@alibaba-inc.com>
2017-05-02 14:24:04 +0800

    test

--- code/js.js
+++ code/js.js
@@ -1,3 +1,7 @@
 console.log('git测试');
 console.log('我是git分支isuss53分支加进去的内容');
 console.log('我是历史的测试内容');
+console.log('我是历史的测试内容');
+console.log('我是历史的测试内容');
+console.log('我是历史的测试内容');
+console.log('我是历史的测试内容');
--- first/first.js
+++ first/first.js
@@ -1,3 +1,2 @@
 console.log('我是修改后的远程分支');
-
 console.log('我是新加入的分支');
