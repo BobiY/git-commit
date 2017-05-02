e11812d3b119171f2dcc53efbfc799c805021ddb
wb-ysb252760 <wb-ysb252760@alibaba-inc.com>
2017-05-02 15:29:35 +0800

    ok

--- first/first.js
+++ first/first.js
@@ -1,12 +1,4 @@
 console.log('我是修改后的远程分支');
-<<<<<<< HEAD
-console.log('我是新加入的分支');
-console.log('我是新加入的分支');
-console.log('我是新加入的分支');
-console.log('我是新加入的分支');
-function ss(){
-=======
 function add(){
->>>>>>> first
     return;
 }
